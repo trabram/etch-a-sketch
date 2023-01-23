4de53ae73db90c9ec083f3d78e8720dde8a545fc
trabram <77090732+trabram@users.noreply.github.com>
2023-01-23 23:21:46 +0700

    Add multiple pen modes

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// init canvas height x width
-let dimension = 16;
+let dimension = 16;   // canvas height x width
+let penMode   = 1;    // default pen mode
 
 // get canvas element
 const elementGrid = document.querySelector('.grid');
+
+function randomHex() {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  return "#" + randomColor;
+}
+
+function RGBToHSL(r, g, b) {
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const l = Math.max(r, g, b);
+  const s = l - Math.min(r, g, b);
+  const h = s
+    ? l === r
+      ? (g - b) / s
+      : l === g
+      ? 2 + (b - r) / s
+      : 4 + (r - g) / s
+    : 0;
+  return [
+    60 * h < 0 ? 60 * h + 360 : 60 * h,
+    100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
+    (100 * (2 * l - s)) / 2,
+  ];
+}
 
 function generateCanvas(size) {
   for (let index = 0; index < size; index++) {
@@ -15,10 +40,20 @@ function generateCanvas(size) {
     for (let i = 0; i < size; i++) {
       let elementBox = document.createElement("div");
       elementBox.classList.add('box');
+      elementBox.style.backgroundColor = 'rgb(255, 255, 255)';
       elementRow.appendChild(elementBox);
 
       // draw on mouse hover
-      elementBox.onmouseover = (e) => e.target.style.backgroundColor = 'black';
+      elementBox.onmouseover = (e) => {
+        if      (penMode == 1) e.target.style.backgroundColor = 'rgb(0, 0, 0)';
+        else if (penMode == 2) e.target.style.backgroundColor = randomHex();
+        else if (penMode == 3) {
+          let color = e.target.style.backgroundColor;
+          let rgb   = color.substring(4, color.length - 1).split(', ');
+          let hsl   = RGBToHSL(rgb[0], rgb[1], rgb[2]);
+          e.target.style.backgroundColor = `hsl(${hsl[0]}, ${hsl[1]}%, ${hsl[2] - 10}%)`
+        }
+      }
     }
     
     // append row to canvas
@@ -44,6 +79,15 @@ function clearCanvas() {
   for (const box of boxes) {
     box.style.backgroundColor = 'white';
   }
+}
+
+function changePen(mode) {
+  const elementMode = document.querySelector('.penmode');
+
+  if (mode == 1) elementMode.innerText = 'Black';
+  if (mode == 2) elementMode.innerText = 'Random';
+  if (mode == 3) elementMode.innerText = 'Gradient';
+  penMode = mode;
 }
 
 generateCanvas(dimension);
