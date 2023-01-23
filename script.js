452f4ebdc7ac86b5ae0ee46fc2c35c646d7b6131
trabram <77090732+trabram@users.noreply.github.com>
2023-01-23 17:05:09 +0700

    Add resize canvas feature

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-// height x width of canvas
+// init canvas height x width
 let dimension = 16;
 
 // get canvas element
-let elementGrid = document.querySelector('.grid');
+const elementGrid = document.querySelector('.grid');
 
-// generate canvas
-for (let index = 0; index < dimension; index++) {
-  
-  // create row element
-  let elementRow = document.createElement("div");
-  elementRow.classList.add('row');
+function generateCanvas(size) {
+  for (let index = 0; index < size; index++) {
+    
+    // create row element
+    let elementRow = document.createElement("div");
+    elementRow.classList.add('row');
 
-  // create box inside row
-  for (let i = 0; i < dimension; i++) {
-    let elementBox = document.createElement("div");
-    elementBox.classList.add('box');
-    elementRow.appendChild(elementBox);
+    // create box inside row
+    for (let i = 0; i < size; i++) {
+      let elementBox = document.createElement("div");
+      elementBox.classList.add('box');
+      elementRow.appendChild(elementBox);
 
-    // draw on mouse hover
-    elementBox.onmouseover = (e) => e.target.style.backgroundColor = 'black';
+      // draw on mouse hover
+      elementBox.onmouseover = (e) => e.target.style.backgroundColor = 'black';
+    }
+    
+    // append row to canvas
+    elementGrid.appendChild(elementRow);
   }
-  
-  // append row to canvas
-  elementGrid.appendChild(elementRow);
 }
+
+function resizeCanvas() {
+  // input new canvas size
+  let newDimension = Number(prompt('Size? (Max 100)'));
+  while (isNaN(newDimension) || newDimension < 1) {
+    newDimension = Number(prompt('Size? (Max 100)'));
+  }
+  if (newDimension > 100) newDimension = 100;
+  
+  // replace canvas
+  elementGrid.innerHTML = '';
+  generateCanvas(newDimension);
+}
+
+generateCanvas(dimension);
